874860a87ed88663437f4e1fb61becb15f59a52f
Letinemeso <sh0n556@bk.ru>
2023-12-20 06:23:37 +0300

    Removed bad design decisions from Data_Extraction_Module, which is probably a bad decision itself

--- include/Data_Extraction_Module.h
+++ include/Data_Extraction_Module.h
@@ -8,42 +8,26 @@
 
 #include <Modules/Physics_Module_2D.h>
 
-#include <Extracted_Data.h>
-
 
 namespace LGui
 {
 
-    class Data_Extraction_Module : public LEti::Module
+    class Data_Extraction_Module : public LEti::Module      //  this class may be pointless
     {
     public:
         DECLARE_VARIABLE;
-
-    public:
-        using Extraction_Function_Type = LST::Function<void(Extracted_Data&)>;
-        using Update_Logic_Function = LST::Function<void(const Extraction_Function_Type&)>;
-
-    private:
-        Extraction_Function_Type m_data_extraction_func;
-        Update_Logic_Function m_update_logic_func;
-
-    public:
-        Data_Extraction_Module();
-        ~Data_Extraction_Module();
 
     private:
         LR::Draw_Module_Base* m_attached_draw_module = nullptr;
         LPhys::Physics_Module_2D* m_attached_physics_module = nullptr;
 
     public:
-        inline void attach_draw_module(LR::Draw_Module_Base* _ptr) { m_attached_draw_module = _ptr; }
-        inline void attach_physics_module(LPhys::Physics_Module_2D* _ptr) { m_attached_physics_module = _ptr; }
-
-        inline void set_data_extraction_func(const Extraction_Function_Type& _data_extraction_func) { m_data_extraction_func = _data_extraction_func; }
-        inline void set_update_logic_func(const Update_Logic_Function& _update_logic_func) { m_update_logic_func = _update_logic_func; }
+        Data_Extraction_Module();
+        ~Data_Extraction_Module();
 
     public:
-        void update(float _dt) override;
+        inline void attach_draw_module(LR::Draw_Module_Base* _ptr) { m_attached_draw_module = _ptr; }
+        inline void attach_physics_module(LPhys::Physics_Module_2D* _ptr) { m_attached_physics_module = _ptr; }
 
     };
 
@@ -53,10 +37,6 @@ namespace LGui
     {
     public:
         DECLARE_VARIABLE;
-
-    public:
-        Data_Extraction_Module::Extraction_Function_Type data_extraction_func;
-        Data_Extraction_Module::Update_Logic_Function update_logic_func;
 
     protected:
         LV::Variable_Base* M_construct_product() const override;
--- source/Data_Extraction_Module.cpp
+++ source/Data_Extraction_Module.cpp
@@ -19,16 +19,6 @@ Data_Extraction_Module::~Data_Extraction_Module()
 
 
 
-void Data_Extraction_Module::update(float /*_dt*/)
-{
-    L_ASSERT(m_data_extraction_func);
-    L_ASSERT(m_update_logic_func);
-
-    m_update_logic_func(m_data_extraction_func);
-}
-
-
-
 INIT_FIELDS(LGui::Data_Extraction_Module_Stub, LV::Builder_Stub)
 FIELDS_END
 
@@ -41,9 +31,4 @@ LV::Variable_Base* Data_Extraction_Module_Stub::M_construct_product() const
 void Data_Extraction_Module_Stub::M_init_constructed_product(LV::Variable_Base* _product) const
 {
     LV::Builder_Stub::M_init_constructed_product(_product);
-
-    Data_Extraction_Module* product = (Data_Extraction_Module*)_product;
-
-    product->set_data_extraction_func(data_extraction_func);
-    product->set_update_logic_func(update_logic_func);
 }
