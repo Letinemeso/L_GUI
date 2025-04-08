3b7029184094ae7143f733106a1a7db576fd6f6c
Артём <sh0n556@bk.ru>
2025-04-08 16:04:06 +0300

    removed autoscaling logic from UI_Object_Stub

--- include/UI_Object_Stub.h
+++ include/UI_Object_Stub.h
@@ -16,36 +16,14 @@ namespace LGui
         INIT_VARIABLE(LGui::UI_Object_Stub, LEti::Object_Stub)
 
         INIT_FIELDS
-        ADD_FIELD(glm::vec3, intended_window_size)
-        ADD_FIELD(std::string, scale_type)
-        ADD_FIELD(glm::vec3, offset)
-        ADD_FIELD(bool, scale_offset_horizontally)
-        ADD_FIELD(bool, scale_offset_vertically)
         ADD_FIELD(LDS::Vector<std::string>, tags)
         FIELDS_END
 
     public:
-        glm::vec3 intended_window_size;
-
-        std::string scale_type = "none";     //  can be "horizontal", "vertical", "both", "proportional"
-
-        glm::vec3 offset{0.0f, 0.0f, 0.0f};
-        bool scale_offset_horizontally = false;
-        bool scale_offset_vertically = false;
-
-    public:
         LDS::Vector<std::string> tags;
 
-    public:
-        UI_Object_Stub();
-        ~UI_Object_Stub();
-
-    private:
-        void M_apply_window_scaling(LEti::Object* _product) const;
-
     protected:
-        LV::Variable_Base* M_construct_product() const override;
-        void M_init_constructed_product(LV::Variable_Base* _product) const override;
+        INIT_BUILDER_STUB(LEti::Object)
 
     };
 
--- source/UI_Object_Stub.cpp
+++ source/UI_Object_Stub.cpp
@@ -3,81 +3,9 @@
 using namespace LGui;
 
 
-UI_Object_Stub::UI_Object_Stub()
+BUILDER_STUB_DEFAULT_CONSTRUCTION_FUNC(UI_Object_Stub)
+
+BUILDER_STUB_INITIALIZATION_FUNC(UI_Object_Stub)
 {
-
-}
-
-UI_Object_Stub::~UI_Object_Stub()
-{
-
-}
-
-
-
-void UI_Object_Stub::M_apply_window_scaling(LEti::Object* _product) const
-{
-    if(intended_window_size.x < 0.000001f && intended_window_size.y < 0.000001f)
-        return;
-
-    glm::vec3 window_ratio = glm::vec3(LR::Window_Controller::instance().get_window_size().x, LR::Window_Controller::instance().get_window_size().y, 0.0f) / intended_window_size;
-
-    glm::vec3 scaled_offset = offset;
-    if(scale_offset_horizontally)
-        scaled_offset.x *= window_ratio.x;
-    if(scale_offset_vertically)
-        scaled_offset.y *= window_ratio.y;
-
-    if(scaled_offset.x < 0.0f)
-        scaled_offset.x += LR::Window_Controller::instance().get_window_size().x;
-    if(scaled_offset.y < 0.0f)
-        scaled_offset.y += LR::Window_Controller::instance().get_window_size().y;
-
-    _product->current_state().move(scaled_offset);
-
-    glm::vec3 scale = _product->current_state().scale();
-
-    if(scale_type == "horizontal")
-    {
-        scale.x *= window_ratio.x;
-    }
-    else if(scale_type == "vertical")
-    {
-        scale.y *= window_ratio.y;
-    }
-    else if(scale_type == "both")
-    {
-        scale.x *= window_ratio.x;
-        scale.y *= window_ratio.y;
-    }
-    else if(scale_type == "proportional")
-    {
-        float multiplier = window_ratio.x;
-        if(window_ratio.y < multiplier)
-            multiplier = window_ratio.y;
-        scale.x *= multiplier;
-        scale.y *= multiplier;
-    }
-    else
-    {
-        L_ASSERT(scale_type == "none");    //  unknown scale type!
-    }
-
-    _product->current_state().set_scale(scale);
-}
-
-
-
-LV::Variable_Base* UI_Object_Stub::M_construct_product() const
-{
-    return new LEti::Object;
-}
-
-void UI_Object_Stub::M_init_constructed_product(LV::Variable_Base *_product) const
-{
-    LEti::Object_Stub::M_init_constructed_product(_product);
-
-    LEti::Object* product = (LEti::Object*)_product;
-
-    M_apply_window_scaling(product);
+    BUILDER_STUB_PARENT_INITIALIZATION;
 }
